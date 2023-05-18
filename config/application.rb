dde0b974b28f7230c6b7a74859a87389546a58ba
Mateus Sousa <mateussdamiano@gmail.com>
2023-05-18 15:36:07 -0300

    try to fix deployment

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,7 @@ module ExpenseTracker
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.assets.paths << "#{Rails.root}/app/assets"
   end
 end
